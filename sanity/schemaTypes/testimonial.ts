ed1173185f43ac99a5dc8df6ed26e4f632dc6d09
harshadh789 <harshadm789@gmail.com>
2026-05-15 02:14:56 +0530

    Add Sanity CMS testimonial schema

---
+++ sanity/schemaTypes/testimonial.ts
@@ -0,0 +1,32 @@
+export default {
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Customer Name',
+      type: 'string',
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'review',
+      title: 'Review',
+      type: 'text',
+    },
+    {
+      name: 'rating',
+      title: 'Rating',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      title: 'Customer Image',
+      type: 'image',
+    },
+  ],
+}
