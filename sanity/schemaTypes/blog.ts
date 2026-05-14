ee77c3b6322046603e04a805dc65fb1b1dfb9760
harshadh789 <harshadm789@gmail.com>
2026-05-15 02:14:25 +0530

    Add Sanity CMS blog schema

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,41 @@
+export default {
+  name: 'blog',
+  title: 'Blog Article',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+  ],
+}
